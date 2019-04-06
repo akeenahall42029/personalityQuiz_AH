faa5282bf8e14f955f0abc84cf3f62424c7090ce
Akeena Hall <akeenadhall@gmail.com>
2019-04-06 15:45:05 -0400

    Figuring out my scoring system

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 var question1 = null;
 var question2 = null;
+var quesion3 = null;
+var question4 = null;
+
 function processResults(){
 	console.log("processResults")
 }
@@ -8,6 +11,27 @@ function getPersonality() {
 }
 function resetQuiz(){
 	console.log("resetQuiz")
+}
+let styleDictionary = {
+	vest: 0;
+	sash: 3;
+};
+let lawDictionary = {
+	honest: 0;
+	friendly: 1;
+	considerate: 2;
+	courageous: 0;
+	responsible: 3;
+};
+let qualityDictionary = {
+	confident: 3;
+	truthful: 2;
+	fincially: 0;
+	ambitious: 1;
+
+};
+let challenges = {
+	
 }
 //Event listeners 
 
